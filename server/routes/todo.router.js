8a2d1afad08cc8a16a3ee56abdd378dec63fe569
Julian Chatterton <julian.james.chatterton@gmail.com>
2024-05-27 23:38:41 -0500

    get route for todo end point is getting DB objects

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -4,8 +4,18 @@ const pool = require('../modules/pool.js');
 
 // GET
 router.get('/', (req, res) => {
-    res.send('test');
-})
+    // res.send('test');
+    let queryText = 'SELECT * FROM "to_dos";';
+    pool.query(queryText)
+        .then((dbResult) => {
+            let toDos = dbResult.rows;
+            console.log(toDos);
+            res.send(toDos);
+        })
+        .catch((dbError => {
+            console.log('Error from GET router:', dbError)
+        }))
+});
 // POST
 
 // PUT
