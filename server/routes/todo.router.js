d028041cbea73e7d4f52fc3b3d615f25397fbda5
Julian Chatterton <julian.james.chatterton@gmail.com>
2024-05-28 00:06:42 -0500

    post route console logs empty 'newTodo' object

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -17,7 +17,22 @@ router.get('/', (req, res) => {
         }))
 });
 // POST
+router.post('/', (req, res) => {
+let newTodo = req.body;
 
+let taskTitle = newTodo.task_title;
+let description = newTodo.description;
+let checkListItems = newTodo.check_list_items;
+let dueDate = newTodo.due_date;
+let isComplete = newTodo.is_complete;
+
+let queryText = `INSERT INTO "to_dos" ("task_title", "description", "check_list_items", "due_date", "is_complete")
+VALUES ($1, $2, $3, $4, $5);`;
+console.log(newTodo);
+res.sendStatus(200);
+
+
+});
 // PUT
 
 // DELETE
