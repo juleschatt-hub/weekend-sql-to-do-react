6e4da79f707d9977a34961127cedf80f683c0990
Julian Chatterton <julian.james.chatterton@gmail.com>
2024-05-31 19:47:55 -0500

    delete route for to_dos by id working

--- server/routes/todo.router.js
+++ server/routes/todo.router.js
@@ -45,8 +45,59 @@ pool.query(queryText, [taskTitle, description, checkListItems, dueDate, isComple
 
 
 });
-// PUT
-
 // DELETE
+router.delete('/:id', (req, res) => {
+    let { id } = req.params;
+    const sqlText = `DELETE FROM "to_dos" WHERE "id" = $1;`;
+    pool.query(sqlText, [id])
+        .then((result) => {
+            console.log(`Got stuff back from the database`, result);
+            res.sendStatus(201);
+        })
+        .catch((error) => {
+            console.log(`Error making database query ${sqlText}`, error);
+            res.sendStatus(500); // Good server always responds
+        })
+});
+// PUT ROUTES
+
+router.put('/toggle/:id', (req, res) => {
+    let { id } = req.params;
+    // This query will switch from true to false and false to true
+    const sqlText = `
+        UPDATE "to_dos" SET "is_complete" = NOT "is_complete" 
+        WHERE "id" = $1;
+    `;
+    pool.query(sqlText, [id])
+        .then((result) => {
+            console.log(`Got stuff back from the database`, result);
+            res.sendStatus(201);
+        })
+        .catch((error) => {
+            console.log(`Error making database query ${sqlText}`, error);
+            res.sendStatus(500); // Good server always responds
+        })
+});
+
+router.put('/:id', (req, res) => {
+    console.log(req.params);
+    let idToupdate = req.params.id;
+    console.log('idToUpdate', idToupdate);
+    console.log('type of idToUpdate', typeof idToupdate);
+    console.log('req.body', req.body);
+    let newDesription = req.body.description;
+    console.log(newDesription);
+    console.log(typeof newDesription);
+    let queryText = `UPDATE treats SET "description"=$1 WHERE id=$2;`;
+
+    pool.query(queryText, [newDesription, idToupdate])
+        .then(dbResult => {
+            console.log('item has been updated:', dbResult);
+            res.sendStatus(200);
+        })
+        .catch(dbError => {
+            console.log('Something went wrong while updating treat: ', dbError)
+        })
+  });
 
 module.exports = router;
